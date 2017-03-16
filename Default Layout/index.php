e1f721ecdd861a72c7ff5c5ed827fdb9e71a7125
Tom Newman <newman2308.spam@hotmail.co.uk>
2017-03-16 14:42:57 +0000

    Website Template
    Starting the project with my default website template

---
+++ Default Layout/index.php
@@ -0,0 +1,23 @@
+<?php include 'inc/header.php' ?>
+
+	<div class="big-banner index-header">
+		<div class="col-md-6 col-md-offset-3">
+			<h1>A Website</h1>
+		</div>
+	</div>
+	
+	<div class="index-content">
+		<div class="container">
+			<div class="row">
+				<div class="intro">
+					<p>A small introduction to the website, appearing above the main content</p>
+				</div>
+			</div>
+			
+			<div class="row">
+				<p>content</p>
+			</div>
+		</div>
+	</div>
+
+<?php include 'inc/footer.php' ?>
